2b8ea6a68e4f4baf7ae85a852832cb0382ddd1e1
Nawfel belgasium <nawfel.bell@gmail.com>
2025-01-09 23:10:26 +0200

    added cors to server

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,15 @@ import express from 'express';
 // import { DbClient } from './db';
 import route from './routes'
 import { errorHandler } from './Middleware/errorHandling';
+import cors from 'cors'
+
 const app = express();
 const port = 3000;
 
 app.use(express.json())
 app.use(route)
 app.use(errorHandler)
+app.use(cors())
 
 app.listen(port, () => {
     console.log(`Server is running on port:${port}`);
