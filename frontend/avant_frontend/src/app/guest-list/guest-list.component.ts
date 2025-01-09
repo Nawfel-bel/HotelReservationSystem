1e581a23820a8fb1cbf15b8e97bbb491f7dc971e
Nawfel belgasium <nawfel.bell@gmail.com>
2025-01-09 15:44:52 +0200

    added angular project

---
+++ frontend/avant_frontend/src/app/guest-list/guest-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-guest-list',
+  imports: [],
+  templateUrl: './guest-list.component.html',
+  styleUrl: './guest-list.component.css'
+})
+export class GuestListComponent {
+
+}
